c6e4fd014af4201e2f4326a38dd66cb0c9cacf2b
jeffsonsenior <kinyuajeff@gmail.com>
2023-10-13 08:21:26 -0700

    header files

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_H_
+#define _SHELL_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/resources.h>
+#include <sys/time.h>
+#include <dirent.h>
+
+
+
+
