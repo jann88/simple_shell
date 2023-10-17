5fe75b25db4645ea1f6ff3da0077c6395f6c7184
jeffsonsenior <kinyuajeff@gmail.com>
2023-10-16 19:59:38 -0700

    tokenizer implementation

--- shell.h
+++ shell.h
@@ -14,6 +14,86 @@
 #include <sys/time.h>
 #include <dirent.h>
 
+char *_getpath(char *command);
+char **tokenizer(char *line);
+char *_getenv(char *variable);
+char *_read_line(void);
+int _execute(char **command, char *argv, int idx);
+
+/* builtins.c */
+int is_builtin(char *command);
 
 
 
+
+/* strings.c */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char _strdup(const char *str);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+/* tools.c */
+void print_error(char *name, char *cmd, int idx);
+void freearray2D(char **array);
+void reverse_string(char *str, int len);
+char *_itoa(int n);
+
+
+
+extern char **environ;
+
+/* environment.c */
+int is_builtin(char **command);
+void handle_builtin(char **command, char **argv, int *status);
+void print_env(char **command, char*status);
+void exit_shell(char *command, char **argv, int *status, int idx);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
