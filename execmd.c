2cf71d12378d7731b01b4bf39c1e0183df67889b
jann88 <jannylynemiaz@gmail.com>
2023-10-18 06:40:38 -0700

    execmd

---
+++ execmd.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * execute - finds builtins and commands
+ * @commandArg: relevant data
+ * Return; 1
+ */
+int execute(param *commandArg)
+{
+	int (*builtin)(param *commandArg);
+
+	if (command->args[0] == NULL)
+	{
+		return (1);
+	}
+	builtin = get_builtin(commandArg->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(commandArg));
+
+	return (cmd_exec(commandArg));
+}
+
