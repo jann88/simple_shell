67f8831b875355eec09e47b0ee83266fb73b2a47
jann88 <jannylynemiaz@gmail.com>
2023-10-18 06:47:09 -0700

    exit code

---
+++ exit.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * exitShell - exits shell
+ * @commandArg: relevance data (status and args)
+ * Return: 0
+ */
+int exitShell(param *commandArg)
+{
+	unisgned int ustatus;
+	int exitCode, arg_len, large_num;
+
+	if (commandArg->args[1] != NULL)
+	{
+		ustatus = _atoi(commandArg->args[1]);
+		exitCode = _isdigit(commandArg->args[1]);
+		arg_len = _strlen(commandArg-args[1]);
+		large_num = ustatus > (unsigned int) INT_MAX;
+		if (!exitCode || arg_len > 10 || large_num)
+		{
+			get_error(commandArg, 2);
+			commandArg->status = 2;
+			return (1);
+		}
+		commandArg->status = (ustatus % 256);
+	}
+	return (0);
+}
