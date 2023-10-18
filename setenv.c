5c7de4ac42f7c5a6f6ea745c91340f63fda59706
jann88 <jannylynemiaz@gmail.com>
2023-10-18 15:05:39 -0700

    setenv

--- setenv.c
+++ setenv.c
@@ -1,169 +1,117 @@
 #include "shell.h"
 /**
- * repeated_char - counts repeated character
- * @input: string input
- * @i: index
- * Return: repetitions
+ * copy_info - copies info to create
+ * a new copy
+ * @name: name
+ * @value: value
+ * Return: new env
  */
-int repeated_char(char *input, int i)
+char *copy_info(char *name, char *value)
 {
-	if (*(input - 1) == *input)
-	{
-		return (repeated_char(input - 1, i + 1));
-	}
-	return (i);
+	char *new;
+	int len_name, len_value, len;
+
+	len_name = _strlen(name);
+	len_value = _strlen(value);
+	len = len_name + len_value + 2;
+	new = malloc(sizeof(char) * (len));
+	-strcpy(new, name);
+	-strcpy(new, "=");
+	-strcpy(new, value);
+	-strcpy(new, '\0');
+
+	return (new);
 }
-
 /**
- *error_sep_op - finds syntax errors
- * @input: string input
- * @i: index
- * @last: last char read
- * Return: index, 0 if otherwise
- */
-int error_sep_op(char *input, int i, char last)
-{
-	int count;
-
-	count = 0;
-	if (*input == '\0')
-		return (0);
-
-	if (*input == ' ' || *input == '\t')
-		return (error_sep_op(input + 1, i + 1, last));
-
-	if (*input == ';')
-		if (last == '|' || last == '&' || last == ';')
-			return (i);
-
-	if (*input == '|')
-	{
-		if (last == ';' || last == '&')
-			return (i);
-
-		if (last == '|')
-		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-				return (i);
-		}
-	}
-
-	if (*input == '&')
-	{
-		if (last == ';' || last == '|')
-			return (i);
-
-		if (last == '&')
-		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-				return (i);
-		}
-	}
-
-	return (error_sep_op(input + 1, i + 1, *input));
-}
-
-/**
- * first_char- finds index of first char
- * @input: string input
- * @i: index
- * Return: 1, 0 if otherwise
- */
-int first_char(char *input, int *i)
-{
-
-	for (*i = 0; input[*i]; *i += 1)
-	{
-		if (input[*i] == ' ' || input[*i] == '\t')
-			continue;
-
-		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
-			return (-1);
-
-		break;
-	}
-
-	return (0);
-}
-
-/**
- * print_syntax_error - prints syntax error when found
+ * set_env - sets environment variable
+ * @name: name
+ * @value: value
  * @commandArg: data structure
- * @input: string input
- * @i: index
- * @bool: to control message error
  * Return: void
  */
-void print_syntax_error(param *commandArg, char *input, int i, int bool)
+void set_env(char *name, char *value, param *commandArg)
 {
-	char *msg, *msg2, *msg3, *error, *counter;
-	int length;
+	int i;
+	char *var_env, *name_env;
 
-	if (input[i] == ';')
+	for (i = 0; commandArg->_environ[i]; i++)
 	{
-		if (bool == 0)
-			msg = (input[i + 1] == ';' ? ";;" : ";");
-		else
-			msg = (input[i - 1] == ';' ? ";;" : ";");
+		var_env = _strdup(commandArg->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, name) == 0)
+		{
+			free(commandArg->_environ[i]);
+			commandArg->_environ[i] = copy_info(name_env, value);
+			free(var_env);
+			return;
+		}
+		free(var_env);
 	}
-
-	if (input[i] == '|')
-		msg = (input[i + 1] == '|' ? "||" : "|");
-
-	if (input[i] == '&')
-		msg = (input[i + 1] == '&' ? "&&" : "&");
-
-	msg2 = ": Syntax error: \"";
-	msg3 = "\" unexpected\n";
-	counter = aux_itoa(commandArg->counter);
-	length = _strlen(commandArg->av[0]) + _strlen(counter);
-	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
-
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
-	{
-		free(counter);
-		return;
-	}
-	_strcpy(error, commandArg->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, counter);
-	_strcat(error, msg2);
-	_strcat(error, msg);
-	_strcat(error, msg3);
-	_strcat(error, "\0");
-
-	write(STDERR_FILENO, error, length);
-	free(error);
-	free(counter);
+	commandArg->_environ = _reallocdp(commandArg->_environ, i, sizeof(char *) * (i + 2));
+	commandArg->_environ[i] = copy_info(name, value);
+	commandArg-> _environ[i + 1] = NULL;
 }
 
 /**
- * check_syntax_error - internediate function to
- * find an dprint synatx error
- * @commandArg: data structure
- * @input: string input
- * Return: 1 for error, 0 if otherwise
+ * _setenv - compares env variables names
+ * @commandArg: relevant data
+ * Return 1
  */
-int check_syntax_error(param *commandArg, char *input)
+int _setenv(param *commandArg)
 {
-	int start_index = 0;
-	int first_char_index = 0;
-	int i = 0;
+	if (commandArg->args[1] == NULL || commandArg[2] == NULL)
+	{
+		get_error(commandArg, -1);
+		return (1);
+	}
+	set_env(commandArg->args[1], commandArg->args[2], commandArg);
+	return (1);
+}
+}
+/**
+ * _unsetenv - deletes an environment variable
+ * @commandArg: relevant data
+ * Return: 1
+ */
+int _unsetenv(param *commandArg)
+{
+	char **realloc_environ;
+	char *var_env, *name_env;
+	int i, j, k;
 
-	first_char_index = first_char(input, &start_index);
-	if (first_char_index == -1)
+	if (commandArg->args[1] == NULL)
 	{
-		print_syntax_error(commandArg, input, start_index, 0);
+		get_error(commandArg, -1);
 		return (1);
 	}
-	i = error_sep_op(input + start_index, 0, *(input + start_index));
-	if ( i != 0)
+	k = -1;
+	for (i = 0; commandArg->_environ[i]; i++)
 	{
-		print_syntax_error(commandArg, input, start_index + i, 1);
+		var_env = _strdup(commandArg->_environ[i]);
+		name_env = _strtok(var_env, "=");
+		if (_strcmp(name_env, commandArg->args[1]) == 0)
+		{
+			k = i;
+		}
+		free(var_env);
+	}
+	if (k == -1)
+	{
+		get_error(commandArg, -1);
 		return (1);
 	}
-	return (0);
+	realloc_environ = malloc(sizeof(char *) * (i));
+	for (i = j = 0; commandArg->_environ[i]; i++)
+	{
+		if (i != k)
+		{
+			realloc_environ[j] = commandArg->_environ[i];
+			j++;
+		}
+	}
+	realloc_environ[j] = NULL;
+	free(commandArg->_environ[k]);
+	free(commandArg->_environ);
+	commandArg->_environ = realloc_environ;
+	return (1);
 }
