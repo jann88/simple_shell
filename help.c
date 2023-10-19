39d219d8215ef9b3a07133bb2e17c81dabd9a0d5
jann88 <jannylynemiaz@gmail.com>
2023-10-18 18:13:48 -0700

    fixing errors

--- help.c
+++ help.c
@@ -1,11 +1,54 @@
 #include "shell.h"
+
 /**
- * displayHelp_env- Help information for env
+ * displayHelp_env - Help information for env.
  * Return: void
  */
 void displayHelp_env(void)
 {
 	char *help = "env: env [option] [name=value] [command [args]]\n\t";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Print the enviroment of the shell.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+
+}
+/**
+ * displayHelp_setenv - Help information for setenv
+ * Return: void
+ */
+void displayHelp_setenv(void)
+{
+
+	char *help = "setenv: setenv (const char *name, const char *value,";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "int replace)\n\t";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Add a new definition to the environment\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+/**
+ * displayHelp_unsetenv - Help information unsetenv
+ * Return: void
+ */
+void displayHelp_unsetenv(void)
+{
+	char *help = "unsetenv: unsetenv (const char *name)\n\t";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Remove an entry completely from the environment\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+
+/**
+ * displayHelp_general - Entry point for help information builtin
+ * Return: void
+ */
+void displayHelp_general(void)
+{
+	char *help = ":)bash, version 1.0(1)-release\n";
 
 	write(STDOUT_FILENO, help, _strlen(help));
 	help = "These commands are defined internally.Type 'help' to see the list";
